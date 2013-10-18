e3d1b379c716b2bcd972178d80cb6e09769c87ed
superpineapple111 <mariusz1605@gmail.com>
2013-10-19 10:17:54 +1100

    Version 1.01

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ welcomescript
 =============
 
 A very simple welcome script
+
+Current welcomescript version: 1.01
+
+Changes:
+
+v1 > 1.01
+Added version number as comment
+Added blank line at the beginning of .bashrc
+Few grammar fixes
--- welcomeinstaller.sh
+++ welcomeinstaller.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+
 echo Installing...
 
+#welcomescript v1.01
+echo "" >> ~/.bashrc
 echo "#--WELCOME SCRIPT--" >> ~/.bashrc
-echo "echo clearing..." >> ~/.bashrc
+echo "welcomescript v1.01" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "echo Clearing..." >> ~/.bashrc
 echo "clear" >> ~/.bashrc
-echo "echo Welcome to $(hostname) $USER" >> ~/.bashrc
+echo "echo Welcome to $(hostname) $USER !" >> ~/.bashrc
 echo "echo " >> ~/.bashrc
 echo "date" >> ~/.bashrc
 echo "echo " >> ~/.bashrc
-echo "echo You are running on kernel $(uname -r)" >> ~/.bashrc
+echo "echo You are running kernel $(uname -r)" >> ~/.bashrc
 echo "echo " >> ~/.bashrc
-echo "echo Enjoy your system" >> ~/.bashrc
+echo "echo Enjoy your terminal" >> ~/.bashrc
 echo "echo " >> ~/.bashrc
-echo "# - END OF WELCOME SCRIPT -" >> ~/.bashrc
+echo "#-- END OF WELCOME SCRIPT--" >> ~/.bashrc
 
 echo Done.
 echo
