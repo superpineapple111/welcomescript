e325f8fe9fe93485eb76d4af02a7bbe0ed7f6b0d
superpineapple111 <mariusz1605@gmail.com>
2013-10-23 18:24:23 +1100

    Version 1.2

--- README.md
+++ README.md
@@ -7,10 +7,23 @@ Current welcomescript version: 1.01
 
 Changes:
 
+v1 > 1.2
+
+-Removed pointless clearing
+
+-Added ASCII art :)
+
+-Added some system information
+
+-Fixed welcomescript not found bug (which was hidden by the clearing)
+
+-Few other minor additions and refinements
+
+
 v1 > 1.01
 
-Added version number as comment
+-Added version number as comment
 
-Added blank line at the beginning of .bashrc
+-Added blank line at the beginning of .bashrc
 
-Few grammar fixes
+-Few grammar fixes
--- welcomeinstaller.sh
+++ welcomeinstaller.sh
@@ -2,22 +2,21 @@
 
 echo Installing...
 
-#welcomescript v1.01
+#welcomescript v1.2
 echo "" >> ~/.bashrc
 echo "#--WELCOME SCRIPT--" >> ~/.bashrc
-echo "welcomescript v1.01" >> ~/.bashrc
+echo "#welcomescript v1.2" >> ~/.bashrc
 echo "" >> ~/.bashrc
-echo "echo Clearing..." >> ~/.bashrc
-echo "clear" >> ~/.bashrc
-echo "echo Welcome to $(hostname) $USER !" >> ~/.bashrc
-echo "echo " >> ~/.bashrc
-echo "date" >> ~/.bashrc
-echo "echo " >> ~/.bashrc
-echo "echo You are running kernel $(uname -r)" >> ~/.bashrc
-echo "echo " >> ~/.bashrc
-echo "echo Enjoy your terminal" >> ~/.bashrc
+echo "echo '      WELCOME!'" >> ~/.bashrc
+echo "echo '     ++++++++++    $(hostname)'" >> ~/.bashrc
+echo "echo '    /          \   $USER'" >> ~/.bashrc
+echo "printf '   |   0    0   |  ' && date" >> ~/.bashrc
+echo "echo '   |            |  ARCHITECTURE: $(uname -i)'" >> ~/.bashrc
+echo "echo '   |  .      .  |  KERNEL: $(uname -r)'" >> ~/.bashrc
+echo "echo '    \  ------  /'" >> ~/.bashrc
+echo "echo '     ++++++++++' " >> ~/.bashrc
 echo "echo " >> ~/.bashrc
 echo "#-- END OF WELCOME SCRIPT--" >> ~/.bashrc
-
-echo Done.
+echo "" >> ~/.bashrc
+echo Done. Restart terminal session to view.
 echo
